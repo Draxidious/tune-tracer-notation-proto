a01f0dcb61395c269db7e9318a4cdfc99f913504
Jordan Muniz <jordy.valois@gmail.com>
2024-04-17 13:10:59 -0400

    Create Modify Rest Skeleton
    - Added a modify rest method and some comments to indicate what it should do

--- src/Measure.js
+++ src/Measure.js
@@ -20,89 +20,116 @@ export class Measure {
         this.height = this.stave.getHeight();
         this.context = context;
         console.log("Context in Measure: " + context);
-        if(timeSignature != "none")
-        {
+        if (timeSignature != "none") {
             this.setTimeSignature(timeSignature);
         }
         console.log("Numbeats: " + this.num_beats);
-        if(clef != "none")
-        {
+        if (clef != "none") {
             this.setClef(clef);
         }
-        let defaultQRest = new this.VF.StaveNote({ keys: ["d/5"], duration: "qr"});
+        let defaultQRest = new this.VF.StaveNote({ keys: ["d/5"], duration: "qr" });
         const notes = [
-            new this.VF.StaveNote({ keys: ["b/4"], duration: "qr"}),
-            new this.VF.StaveNote({ keys: ["b/4"], duration: "qr"}),
-            new this.VF.StaveNote({ keys: ["b/4"], duration: "qr"}),
-            new this.VF.StaveNote({ keys: ["b/4"], duration: "qr"})
+            new this.VF.StaveNote({ keys: ["b/4"], duration: "qr" }),
+            new this.VF.StaveNote({ keys: ["b/4"], duration: "qr" }),
+            new this.VF.StaveNote({ keys: ["b/4"], duration: "qr" }),
+            new this.VF.StaveNote({ keys: ["b/4"], duration: "qr" })
         ];
 
-        this.voice1 = new this.VF.Voice({ num_beats: this.num_beats, beat_value: this.beat_value}).addTickables(notes);
+        this.voice1 = new this.VF.Voice({ num_beats: this.num_beats, beat_value: this.beat_value }).addTickables(notes);
+        notes.forEach(function (note) {
+            console.log(note.getTicks());
+        });
         this.stave.setContext(context).draw();
         this.renderVoices();
     }
 
-    setTimeSignature = function(/*string*/ timeSignature) {
+    setTimeSignature = function (/*string*/ timeSignature) {
         this.stave.setTimeSignature(timeSignature);
         let arr = timeSignature.split("/");
         this.num_beats = parseInt(arr[0]);
         this.beat_value = parseInt(arr[1]);
     }
 
-    setClef = function(/*string*/ clef) {
+    setClef = function (/*string*/ clef) {
         this.stave.setClef(clef);
     }
 
-    addNote = function(/*string[]*/keys, /*string*/ duration, /*string*/ noteId)
-    {
+    addNote = function (/*string[]*/keys, /*string*/ duration, /*string*/ noteId) {
+
         let VF = Vex.Flow;
         let notes = [];
-        
-        this.voice1.getTickables().forEach(function (staveNote){
-           console.log(staveNote);
-           if(staveNote.attrs.id == noteId)
-           {
-             // If staveNote is not a rest, then we add to the chord
-             if(staveNote.noteType != 'r')
-             {
-                let newKeys = staveNote.getKeys();
-                keys.forEach(function(key){
-                    // We don't want repeat keys
-                    if(!newKeys.includes(key)) newKeys.push(key);
-                 });
-                notes.push( new VF.StaveNote({ keys: newKeys, duration: duration}));
-             }
-             // If the staveNote is a rest, then we replace it
-             else
-             {
-                // Replace since its a rest
-                notes.push( new VF.StaveNote({ keys: keys, duration: duration}));
-             } 
-             
-           }
-           else
-           {
-             notes.push(staveNote);
-           }
-           let svgNote = document.getElementById('vf-'+staveNote.attrs.id);
-           //console.log(staveNote.attrs.id);
-           svgNote.remove();
+
+        this.voice1.getTickables().forEach(function (staveNote) {
+            console.log(staveNote);
+            // If invalid duration nothing changes
+            if (staveNote.attrs.id == noteId && duration == staveNote.duration) {
+                if (duration[duration.length - 1] == 'r') {
+                    // Handle replacing all notes with the desired rest
+                }
+                else {
+                    // If staveNote is not a rest, then we add to the chord
+                    if (staveNote.noteType != 'r') {
+                        let newKeys = staveNote.getKeys();
+                        keys.forEach(function (key) {
+                            // We don't want repeat keys
+                            if (!newKeys.includes(key)) newKeys.push(key);
+                        });
+                        notes.push(new VF.StaveNote({ keys: newKeys, duration: duration }));
+                    }
+                    // If the staveNote is a rest, then we replace it
+                    else {
+                        // Replace since its a rest
+                        notes.push(new VF.StaveNote({ keys: keys, duration: duration }));
+                    }
+                }
+            }
+            else {
+                notes.push(staveNote);
+            }
+            let svgNote = document.getElementById('vf-' + staveNote.attrs.id);
+            //console.log(staveNote.attrs.id);
+            svgNote.remove();
         });
-        this.voice1 = new VF.Voice({ num_beats: this.num_beats, beat_value: this.beat_value}).addTickables(notes);
+        this.voice1 = new VF.Voice({ num_beats: this.num_beats, beat_value: this.beat_value }).addTickables(notes);
         this.renderVoices();
         // When adding a note you never want to override another note
         // However, if the StaveNote you are overriding is at REST, then override
     }
 
-    getCurrentBeats = function()
-    {
+    modifyRest = function (duration, /*string*/ noteId) {
+
+        if (duration[duration.length - 1] != 'r') {
+            // We don't add notes here
+            return;
+        }
+        let VF = Vex.Flow;
+        let notes = [];
+        let originalTicks = this.voice1.getTicks();
+        this.voice1.getTickables().forEach(function (staveNote) {
+            if (staveNote.attrs.id == noteId) {
+
+
+            }
+            else {
+                notes.push(staveNote);
+            }
+            let svgNote = document.getElementById('vf-' + staveNote.attrs.id);
+            //console.log(staveNote.attrs.id);
+            svgNote.remove();
+        });
+        // We replace the desired rest with a series of new rests that represent the new duration
+        // We then count the Ticks in this new set of notes, if there are too many or too few its invalid
+        // ^ AKA if the newTicks != oldTicks then invalid
+
+    }
+
+    getCurrentBeats = function () {
         return this.num_beats;
     }
 
-    renderVoices = function()
-    {
+    renderVoices = function () {
         new this.VF.Formatter().format([this.voice1], this.width - 25);
         this.voice1.draw(this.context, this.stave);
     }
 
-  };
+};
